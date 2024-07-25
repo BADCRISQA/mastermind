f05527e5a3b19ba91dab77e7fcd90f5f9fce85d2
BADCRISQA <89713869+BADCRISQA@users.noreply.github.com>
2024-07-25 14:58:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
 import random
 
-class Mastermind:
-    def __init__(self, colors=["R", "G", "B", "Y", "O", "P"], code_length=4):
-        self.colors = colors
-        self.code_length = code_length
+class MastermindBoard:
+    COLORS = ['R', 'G', 'B', 'Y', 'O', 'P']  # Red, Green, Blue, Yellow, Orange, Purple
+    
+    def __init__(self):
         self.code = self.generate_code()
         self.guesses = []
-        self.feedback = []
+        self.feedbacks = []
+        self.max_attempts = 10
 
     def generate_code(self):
-        return [random.choice(self.colors) for _ in range(self.code_length)]
+        return [random.choice(self.COLORS) for _ in range(4)]
 
-    def generate_feedback(self, guess):
-        feedback = []
+    def make_guess(self, guess):
+        if len(guess) != 4 or not all(color in self.COLORS for color in guess):
+            raise ValueError("Guess must be a list of 4 colors from: " + ", ".join(self.COLORS))
+        
+        self.guesses.append(guess)
+        feedback = self.get_feedback(guess)
+        self.feedbacks.append(feedback)
+        
+        return feedback
+    
+    def get_feedback(self, guess):
+        black_pegs = 0
+        white_pegs = 0
         code_copy = self.code[:]
         guess_copy = guess[:]
 
-        for i in range(self.code_length):
-            if guess_copy[i] == code_copy[i]:
-                feedback.append("B")
+        # First pass: count black pegs and mark them
+        for i in range(4):
+            if guess[i] == code_copy[i]:
+                black_pegs += 1
                 code_copy[i] = guess_copy[i] = None
-
-        for i in range(self.code_length):
+        
+        # Second pass: count white pegs
+        for i in range(4):
             if guess_copy[i] is not None and guess_copy[i] in code_copy:
-                feedback.append("W")
+                white_pegs += 1
                 code_copy[code_copy.index(guess_copy[i])] = None
+        
+        return {'black': black_pegs, 'white': white_pegs}
+    
+    def is_solved(self):
+        return any(feedback['black'] == 4 for feedback in self.feedbacks)
 
-        feedback.sort(reverse=True)
-        return feedback
+    def is_game_over(self):
+        return self.is_solved() or len(self.guesses) >= self.max_attempts
 
-    def make_guess(self, guess):
-        if len(guess) != self.code_length:
-            raise ValueError("Guess must be the same length as the code.")
-        self.guesses.append(guess)
-        self.feedback.append(self.generate_feedback(guess))
-        return self.feedback[-1]
+    def print_board(self):
+        for i, guess in enumerate(self.guesses):
+            feedback = self.feedbacks[i]
+            print(f"Guess {i+1}: {guess} -> Black Pegs: {feedback['black']}, White Pegs: {feedback['white']}")
+
+    def reset_game(self):
+        self.code = self.generate_code()
+        self.guesses = []
+        self.feedbacks = []
